33fc594ca46f6467d60274afb2f168e5b76e1227
michaelgeerts <geertsmichael@gmail.com>
2016-01-09 19:49:52 +0100

    Testing platform

--- index.js
+++ index.js
@@ -7,8 +7,36 @@ module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory("homebridge-loxone", "Loxone", Loxone);
+    //homebridge.registerAccessory("homebridge-loxone", "Loxone", Loxone);
+
+    homebridge.registerPlatform("homebridge-loxone", "Loxone", LoxonePlatform);
 };
+
+function LoxonePlatform(log, config) {
+    this.log = log;
+    this.debug = log.debug;
+    this.ip_address = config['ip_address'];
+    this.username = config['username'];
+    this.password = config['password'];
+}
+
+LoxonePlatform.prototype = {
+    accessories: function(callback) {
+        this.log("Fetching LoxonePlatform accessories.");
+
+        //create array of accessories
+        var myAccessories = [];
+
+        this.log(this.ip_address);
+
+        myAccessories.push(new Loxone(this.log, this));
+
+        // if done, return the array to callback function
+        callback(myAccessories);
+    }
+};
+
+
 
 function Loxone(log, config) {
     this.log = log;
